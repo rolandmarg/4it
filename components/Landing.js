076264bb0e7dceb18de6227e6bcde3cac2467b30
Rom Midnight <rmargve@gmail.com>
2020-08-25 04:30:42 +0400

    fix modal, add subscribe card on landing page

--- components/Landing.js
+++ components/Landing.js
@@ -1,3 +1,4 @@
+import { useFormik } from 'formik';
 import Modal from './Modal';
 import Testimonials from './Testimonials';
 import useVisible from './UseVisible';
@@ -10,14 +11,185 @@ export default function Landing({ testimonials }) {
     </>
   );
 }
+const SignupForm = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor='email'>Email Address</label>
+      <input
+        id='email'
+        name='email'
+        type='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+};
 
-function Banana() {
+function Subscribe() {
+  const { isVisible, setIsVisible, ref } = useVisible(false);
+  return (
+    <div className=''>
+      <button className='btn btn-black m-2 w-32' onClick={() => setIsVisible(true)}>
+        Subscribe
+      </button>
+      <Modal isVisible={isVisible}>
+        <div ref={ref}>
+          <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+            <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+              <h3
+                className='text-lg leading-6 font-medium text-gray-900'
+                id='modal-headline'
+              >
+                Subscribe
+              </h3>
+            </div>
+            <div className='m-2'>
+              {/* <SignupForm /> */}
+            </div>
+          </div>
+          <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+            <span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
+              <button
+                type='button'
+                className='inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-teal-400 text-base leading-6 font-medium text-white shadow-sm hover:bg-teal-500 focus:outline-none focus:border-teal-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+              >
+                Subscribe
+              </button>
+            </span>
+            <span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
+              <button
+                type='button'
+                onClick={() => setIsVisible(false)}
+                className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+              >
+                Cancel
+              </button>
+            </span>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+function ForEmployers() {
   const { isVisible, setIsVisible, ref } = useVisible(false);
 
   return (
-    <main id='about' className='bg-yellow-200 p-2 md:h-screen'>
+    <div className='bg-yellow-50 rounded p-2 m-2 w-72 mx-auto'>
+      <h1 className='text-2xl font-semibold m-2 mb-4'>For Employers</h1>
+      <div className='flex justify-center'>
+        <button
+          onClick={() => setIsVisible(true)}
+          className='m-2 w-56 btn btn-black'
+        >
+          Schedule Meeting
+        </button>
+        <Modal isVisible={isVisible}>
+          <div ref={ref}>
+            <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+              <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+                <h3
+                  className='text-lg leading-6 font-medium text-gray-900'
+                  id='modal-headline'
+                >
+                  Schedule Meeting
+                </h3>
+              </div>
+            </div>
+            <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+              <span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
+                <button
+                  type='button'
+                  className='inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-teal-400 text-base leading-6 font-medium text-white shadow-sm hover:bg-teal-500 focus:outline-none focus:border-teal-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+                >
+                  Schedule
+                </button>
+              </span>
+              <span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
+                <button
+                  type='button'
+                  onClick={() => setIsVisible(false)}
+                  className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+                >
+                  Cancel
+                </button>
+              </span>
+            </div>
+          </div>
+        </Modal>
+      </div>
+    </div>
+  );
+}
+
+function ForJobSeekers() {
+  const { isVisible, setIsVisible, ref } = useVisible(false);
+
+  return (
+    <div className='bg-yellow-50 rounded p-2 m-2 w-72 mx-auto'>
+      <h1 className='text-2xl font-semibold m-2 mb-4'>For Job Seekers</h1>
+      <div className='flex justify-center'>
+        <button
+          onClick={() => setIsVisible(true)}
+          className='m-2 w-56 btn btn-black'
+        >
+          Upload Resume
+        </button>
+        <Modal isVisible={isVisible}>
+          <div ref={ref}>
+            <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+              <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+                <h3
+                  className='text-lg leading-6 font-medium text-gray-900'
+                  id='modal-headline'
+                >
+                  Upload Resume
+                </h3>
+              </div>
+            </div>
+            <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+              <span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
+                <button
+                  type='button'
+                  className='inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-teal-400 text-base leading-6 font-medium text-white shadow-sm hover:bg-teal-500 focus:outline-none focus:border-teal-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+                >
+                  Upload
+                </button>
+              </span>
+              <span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
+                <button
+                  type='button'
+                  onClick={() => setIsVisible(false)}
+                  className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+                >
+                  Cancel
+                </button>
+              </span>
+            </div>
+          </div>
+        </Modal>
+      </div>
+    </div>
+  );
+}
+function Banana() {
+  return (
+    <main id='about' className='bg-yellow-200 p-2 xl:h-screen'>
       <div
-        className='p-2 mt-16 md:m-32 lg:mt-36 max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl flex flex-col
+        className='p-2 mt-16 md:m-16 lg:mt-36 max-w-md sm:max-w-2xl md:max-w-3xl lg:max-w-4xl flex flex-col
         sm:space-x-4 sm:flex-row'
       >
         <div className='p-4'>
@@ -39,65 +211,18 @@ function Banana() {
           </p>
         </div>
       </div>
+
       <div
-        className='flex flex-col rounded max-w-sm mx-auto m-2 md:my-12 lg:mt-36 px-2 justify-center 
+        className='flex flex-col rounded max-w-sm mx-auto m-2 lg:mt-24 px-2 justify-center 
         sm:flex-row sm:space-x-4 sm:max-w-md sm:bg-teal-300'
       >
-        <div className='bg-yellow-50 rounded p-2 m-2 w-72 mx-auto'>
-          <h1 className='text-2xl font-semibold m-2 mb-4'>For Employers</h1>
-          <div className='flex justify-center'>
-            <button className='m-2 w-56 btn btn-black'>Schedule Meeting</button>
-          </div>
-        </div>
-        <div className='bg-yellow-50 rounded p-2 m-2 w-72 mx-auto'>
-          <h1 className='text-2xl font-semibold m-2 mb-4'>For Job Seekers</h1>
-          <div className='flex justify-center'>
-            <button
-              onClick={() => setIsVisible(true)}
-              className='m-2 w-56 btn btn-black'
-            >
-              Upload Resume
-            </button>
-            <Modal isVisible={isVisible}>
-              <div
-                ref={ref}
-                className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
-                role='dialog'
-                aria-modal='true'
-                aria-labelledby='modal-headline'
-              >
-                <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
-                  <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
-                    <h3
-                      className='text-lg leading-6 font-medium text-gray-900'
-                      id='modal-headline'
-                    >
-                      Upload Resume
-                    </h3>
-                  </div>
-                </div>
-                <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
-                  <span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
-                    <button
-                      type='button'
-                      className='inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-teal-400 text-base leading-6 font-medium text-white shadow-sm hover:bg-teal-500 focus:outline-none focus:border-teal-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
-                    >
-                      Upload
-                    </button>
-                  </span>
-                  <span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
-                    <button
-                      type='button'
-                      onClick={() => setIsVisible(false)}
-                      className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5'
-                    >
-                      Cancel
-                    </button>
-                  </span>
-                </div>
-              </div>
-            </Modal>
-          </div>
+        <ForEmployers />
+        <ForJobSeekers />
+      </div>
+      <div className='bg-yellow-50 rounded p-2 m-2 my-4 w-72 mx-auto'>
+        <h1 className='text-2xl font-semibold m-2 mb-4'>For You</h1>
+        <div className='flex justify-center'>
+          <Subscribe />
         </div>
       </div>
     </main>
--- components/Modal.js
+++ components/Modal.js
@@ -26,7 +26,14 @@ export default function Modal({ children, isVisible }) {
           leaveFrom='opacity-100 translate-y-0 sm:scale-100'
           leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
         >
-          {children}
+          <div
+            className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'
+            role='dialog'
+            aria-modal='true'
+            aria-labelledby='modal-headline'
+          >
+            {children}
+          </div>
         </Transition>
       </div>
     </Transition>
